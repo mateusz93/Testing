e83cd36fd2c533163032ecbc124a0c558c06b8eb
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-04-06 21:55:27 +0200

    Dodanie brakującej implementacji do testu

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,14 +3,24 @@ package edu.iis.mto.bsearch;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+
 /**
  * @Author Mateusz Wieczorek, 06.04.16.
  */
 public class BinarySearchTest {
 
     @Test
-    public void checkIfKeyIsInSequence() {
-
+    public void checkWhetherKeyIsInASequence() {
+        int[] sequence = {13};
+        int key = 13;
+        SearchResult searchResult = BinarySearch.search(key, sequence);
+        int position = searchResult.getPosition();
+        assertThat(searchResult.isFound(), is(true));
+        assertThat(position, is(not(-1)));
+        assertThat(sequence[position] == key, is(true));
     }
 
 }
