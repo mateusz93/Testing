b54d49203d978027ad28d8b9836c9cd75e46a960
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-04-06 21:48:02 +0200

    Dodanie testu sprawdzającego czy element jest w sekwencji

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,16 @@
+package edu.iis.mto.bsearch;
+
+
+import org.junit.Test;
+
+/**
+ * @Author Mateusz Wieczorek, 06.04.16.
+ */
+public class BinarySearchTest {
+
+    @Test
+    public void checkIfKeyIsInSequence() {
+
+    }
+
+}
