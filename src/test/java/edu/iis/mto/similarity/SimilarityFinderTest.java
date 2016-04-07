75fab0bdbd978eebd0ec40f909a3a343b4dc88f0
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-04-07 14:06:12 +0200

    Dodanie testu sprawdzającego czy wartość zwracana w przypadku pustych sekwencji = 0

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,30 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Mateusz on 2016-04-07.
+ */
+public class SimilarityFinderTest {
+
+    private SequenceSearcher searcher;
+
+    @Before
+    public void setUp() {
+        searcher = null;
+    }
+
+    @Test
+    public void sequenceLenghtTest() {
+        SimilarityFinder finder = new SimilarityFinder(searcher);
+        int[] seq1 = {};
+        int[] seq2 = {};
+        assertThat(finder.calculateJackardSimilarity(seq1, seq2), is(1.0d));
+    }
+
+}
