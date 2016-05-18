402233e81a8831ae243db143b255a43a4a5438fa
Mateusz <187835@edu.p.lodz.pl>
2016-05-18 18:29:22 +0200

    [Green] balancing server with one slot capacity and one virtual machine

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,7 +4,10 @@ package edu.iis.mto.serverloadbalancer;
  * @Author Mateusz Wieczorek, 18.05.16.
  */
 public class ServerLoadBalancer {
-    public void balance(Server[] servers, VirtualMachine[] virtualMachines) {
 
+    public void balance(Server[] servers, VirtualMachine[] virtualMachines) {
+        if (virtualMachines.length > 0) {
+            servers[0].currentLoadPercentage = 100.0d;
+        }
     }
 }
