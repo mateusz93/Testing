dc6b1a22a5b2605e4367d1da9fc2877e7c0e257c
Mateusz <187835@edu.p.lodz.pl>
2016-06-15 18:35:25 +0200

    [Green] test 3

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -6,12 +6,17 @@ package edu.iis.mto.serverloadbalancer;
 public class Server {
 
     public double currentLoadPercentage;
+    private int capacity;
 
     public Server(int capacity) {
-
+        this.capacity = capacity;
     }
 
     public boolean contains(Vm vm) {
         return true;
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 }
