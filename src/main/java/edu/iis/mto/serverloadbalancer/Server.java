d33c70dbc36479913e8ebf3d4446c4ddc70c6f60
Mateusz <187835@edu.p.lodz.pl>
2016-06-15 19:50:14 +0200

    [Green] test 6

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -21,15 +21,26 @@ public class Server {
         this.capacity = capacity;
         this.currentLoadPercentage = loadedLevel;
         vms = new ArrayList<Vm>();
+        for (int i = 0; i < currentLoadPercentage / capacity; ++i) {
+            Vm vm = new Vm(1);
+            vms.add(vm);
+        }
     }
 
     public boolean contains(Vm vm) {
-        return true;
+        for (Vm v : vms) {
+            if (v == vm) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void addVm(Vm vm) {
-        this.currentLoadPercentage = vm.size / (double) capacity * 100.0d;
-        vms.add(vm);
+        if (this.currentLoadPercentage + vm.size / (double) capacity * 100.0d <= 100.0d) {
+            this.currentLoadPercentage += vm.size / (double) capacity * 100.0d;
+            vms.add(vm);
+        }
     }
 
     public int getCapacity() {
