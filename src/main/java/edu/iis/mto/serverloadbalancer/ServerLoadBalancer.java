a81b64f6543d150fd02815d6c303a79a4728936d
Mateusz <187835@edu.p.lodz.pl>
2016-06-15 18:59:33 +0200

    [Refactorization] test 4

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -6,10 +6,8 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalancer {
 
     public void balance(Server[] servers, Vm[] vms) {
-        if (vms.length > 0 && servers.length == 1) {
-            for (Vm vm : vms) {
-                servers[0].addVm(vm);
-            }
+        for (Vm vm : vms) {
+            servers[0].addVm(vm);
         }
     }
 }
